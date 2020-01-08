6b418e7806cc26b9a18fa453744690d478ff9836
diegodnz <diegodiniz991@gmail.com>
2020-01-08 20:57:20 -0200

    'blog_path

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def hello_blog(request):
+    numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+    data = {'diego': 'diniz', 'numeros': numeros}
+    return render(request, 'index.html', data)
 # Create your views here.
