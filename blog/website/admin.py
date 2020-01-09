bff5240ae3acd9fef0a04a15d2acc6a037caedce
diegodnz <diegodiniz991@gmail.com>
2020-01-09 15:28:07 -0200

    'adminchanges'

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subtitle', 'content']
+    search_fields = ['title', 'subtitle', 'content']
+    fields = ['title', 'subtitle', 'content']
+admin.site.register(Post, PostAdmin)
 # Register your models here.
--- blog/website/models.py
+++ blog/website/models.py
@@ -4,3 +4,9 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=200)
     content = models.TextField()
+    def __str__(self):
+        return self.title
+    def title_subtitle(self):
+        return self.title + ' - ' + self.content
+    title_subtitle.admin_order_field = 'title'
+
