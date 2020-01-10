4d2e4d6b74dc750febecc549291cca7172b0948e
diegodnz <diegodiniz991@gmail.com>
2020-01-10 15:04:22 -0200

    'render_media'

--- blog/website/models.py
+++ blog/website/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
         choices = Categories.choices,
         default = 'GR'
     )
+    image = models.ImageField(upload_to='posts', null=True, blank=True)
 
     def __str__(self):
         return self.title
